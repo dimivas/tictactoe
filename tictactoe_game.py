5c0826057ac515c65799c85b537f733fd89da77c
Demetrios Vassiliades <dimivas@ieee.org>
2017-01-31 21:41:24 +0200

    Refactored in order to declare game board size and winning size as variable

--- tictactoe_game.py
+++ tictactoe_game.py
@@ -11,8 +11,9 @@ class TicTacToeGame(object):
     RESULT_DRAW = None
 
 
-    def __init__(self, p1, p2, board_size=3, player_symbol=('X', 'O')):
+    def __init__(self, p1, p2, board_size=(6, 7), win_size=4, player_symbol=('X', 'O')):
         self.board_size = board_size
+        self.win_size = win_size
         self.player_symbol = player_symbol
         self.players = (p1, p2)
         self.game_id = 0
@@ -24,7 +25,7 @@ class TicTacToeGame(object):
         """
         Initialize game board with default value
         """
-        self.board = map(lambda x: [None] * self.board_size, range(self.board_size))
+        self.board = map(lambda x: [None] * self.board_size[1], range(self.board_size[0]))
 
 
     def print_board(self):
@@ -32,8 +33,8 @@ class TicTacToeGame(object):
         Prints the game board with the current state
         """
         os.system('clear')
-        print("  {}".format(" ".join(str(x) for x in range(self.board_size))))
-        for line_id in range(self.board_size):
+        print("  {}".format(" ".join(str(x) for x in range(self.board_size[1]))))
+        for line_id in range(self.board_size[0]):
             line_state = " ".join(map(lambda x: x and str(x) or '-', self.board[line_id]))
             print("{} {}".format(line_id, line_state))
         print()
@@ -66,14 +67,44 @@ class TicTacToeGame(object):
         else:
             row = int(row)
             col = int(col)
-            if (row < 0 or row >= self.board_size):
+            if (row < 0 or row >= self.board_size[0]):
                 result = False
-            if (col < 0 or col >= self.board_size):
+            if (col < 0 or col >= self.board_size[1]):
                 result = False
         if (result == True):
             if self.__is_seat_occupied(row, col):
                 result = False
         return result
+
+
+    def __have_we_a_winner_in_row(self, row):
+        """
+        """
+        result = False
+        if (row[0] and all(map(lambda x: x == row[0], row))):
+            result = True
+        return result
+
+
+    def __have_we_a_winner_in_square(self, start_row, start_col):
+        """
+        """
+        # Left to right cross
+        left_cross = map(lambda x: self.board[start_row + x][start_col + x], range(self.win_size))
+        if (self.__have_we_a_winner_in_row(left_cross)):
+            return True
+        # Right to left cross
+        right_cross = map(lambda x: self.board[start_row + x][start_col + self.win_size - 1 - x], range(self.win_size))
+        if (self.__have_we_a_winner_in_row(right_cross)):
+            return True
+        for i in range(self.win_size):
+            offset_row = self.board[i + start_row][start_col:start_col + self.win_size]
+            if (self.__have_we_a_winner_in_row(offset_row)):
+                return True
+            offset_col = map(lambda x: x[i + start_col], self.board[start_row:start_row + self.win_size])
+            if (self.__have_we_a_winner_in_row(offset_col)):
+                return True
+        return False
 
 
     def __have_we_a_winner(self, last_move):
@@ -82,18 +113,18 @@ class TicTacToeGame(object):
         @param last_move (tuple): tuple/list with row and column number of the last move (e.g. (1, 2))
         @return (bool)
         """
-        result = False
-        row, col = last_move
-        for i in range(self.board_size):
-            if (self.board[i][0] and self.board[i][0] == self.board[i][1] and self.board[i][0] == self.board[i][2]):
-                result = True
-            if (self.board[0][i] and self.board[0][i] == self.board[1][i] and self.board[0][i] == self.board[2][i]):
-                result = True
-        if (self.board[0][0] and self.board[0][0] == self.board[1][1] and self.board[0][0] ==self. board[2][2]):
-            result = True
-        if (self.board[2][0] and self.board[2][0] == self.board[1][1] and self.board[2][0] == self.board[0][2]):
-            result = True
-        return result
+        x_start = max(last_move[0] - (self.win_size - 1), 0)
+        x_end = min(last_move[0] + (self.win_size - 1), self.board_size[0] - 1)
+
+        y_start = max(last_move[1] - (self.win_size - 1), 0)
+        y_end = min(last_move[1] + (self.win_size - 1), self.board_size[1] - 1)
+
+        for i in range(x_start, x_end - (self.win_size - 2)):
+            for j in range(y_start, y_end - (self.win_size - 2)):
+                if (self.__have_we_a_winner_in_square(i, j)):
+                    return True
+
+        return False
 
 
     def __end_of_game(self, winning_player, be_verbose=True):
@@ -114,10 +145,11 @@ class TicTacToeGame(object):
         @param be_verbose (bool): Boolean for being verbose or not
         """
         map(lambda x: self.players[x].set_player_id(self.player_symbol[x]), range(len(self.players)))
+
         round = 0
         while(True):
             be_verbose and self.print_board()
-            if (round > 8):
+            if (round >= self.board_size[0] * self.board_size[1]):
                 be_verbose and print("Game {}: This is a draw!".format(self.game_id))
                 map(lambda x: x.end_of_game(self.RESULT_DRAW), self.players)
                 break
