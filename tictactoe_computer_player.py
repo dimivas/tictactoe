97f3147f9d160994bc776be1dd6d8a6598e0083e
Demetrios Vassiliades <dimivas@ieee.org>
2017-02-25 13:21:47 +0200

    Adding documentation

--- tictactoe_computer_player.py
+++ tictactoe_computer_player.py
@@ -1,7 +1,9 @@
+"""
+The implementation of the Tic-Tac-Toe player component using Q-Learning
+"""
 from __future__ import print_function
 
 import random
-import time
 
 from tictactoe_player import TicTacToePlayer
 
@@ -18,6 +20,14 @@ class TicTacToeCComputerPlayer(TicTacToePlayer):
 
 
     def __init__(self, alpha=0.99, epsilon=1.0, epsilon_decay_step=10e-5):
+        """
+        Constructor
+
+        @param alpha: the alpha parameter (learning rate) of the Q-Learning algorithm (0, 1]
+        @param epsilon: the epsilon parameter (randomness of the next move)
+                        of Q-Learning algorithm [0, 1]
+        @param epsilon_decay_step: the decay factor for updating the epsilon parameter [0, 1]
+        """
         self.alpha = alpha
         self.epsilon = epsilon
         self.epsilon_decay_step = epsilon_decay_step
@@ -28,18 +38,38 @@ class TicTacToeCComputerPlayer(TicTacToePlayer):
 
 
     def __apply_move_on_state(self, game_state, move):
+        """
+        Apply move on the current game state
+
+        @param game_state: a 2D list with the game state given by the game engine
+        @param move: a list with the x and y values of the move  
+        @return: a 2D list with the merged result
+        """
         next_game_state = [x[:] for x in game_state]
         next_game_state[move[0]][move[1]] = self.player_id
         return next_game_state
 
 
     def __encode_state(self, game_state):
+        """
+        Transforms the 2D list game state into an internal representation
+
+        @param game_state: a 2D list with the game state given by the game engine
+        @return: a (flatten) list with the internal representation of the game engine
+        """
         flatten_list = list(item for sublist in game_state for item in sublist)
         encoded_state = tuple(map(self.__map_player_id, flatten_list))
         return encoded_state
 
 
     def __get_free_seats(self, game_state):
+        """
+        Get the available (free) seats
+
+        @param game_state: a 2D list with the game state
+        @return: a list with the set of the available seats.
+                 Each seat is a tuple with the x and y values.
+        """
         free_seats = []
         for i in range(len(game_state)):
             for j in range(len(game_state[i])):
@@ -49,6 +79,12 @@ class TicTacToeCComputerPlayer(TicTacToePlayer):
 
 
     def __get_next_greedy_move(self, game_state):
+        """
+        Get the next move based on a greedy algorithm and the Q-Values
+
+        @param game_state: a 2D list with the game state
+        @return: a list with the x and y values of the selected seat
+        """ 
         best_move = None
         best_score = None
         for free_seat in self.__get_free_seats(game_state):
@@ -64,15 +100,34 @@ class TicTacToeCComputerPlayer(TicTacToePlayer):
 
 
     def __get_next_random_move(self, game_state):
+        """
+        Get the next move based on a random selection
+
+        @param game_state: a 2D list with the game state
+        @return: a list with the x and y values of the selected seat
+        """
         return random.choice(self.__get_free_seats(game_state))
 
 
     def __get_score(self, game_state, move):
+        """
+        Get the Q-Value of the move
+
+        @param game_state: a 2D list with the game state
+        @move: a list with the x and y values of the selected move
+        @return: the Q-Value
+        """
         next_game_state = self.__apply_move_on_state(game_state, move)
         return self.q_values[self.__encode_state(next_game_state)]
 
 
     def __init_q_values(self, game_state):
+        """
+        Initialize the Q-Values for the current game state and 
+        the next possible game states, based on the available seats
+        
+        @param game_state: a 2D list with the game state
+        """
         encoded_state = self.__encode_state(game_state)
         if encoded_state not in self.q_values:
             self.q_values[encoded_state] = self.INITIAL_STATE_VALUE
@@ -83,41 +138,69 @@ class TicTacToeCComputerPlayer(TicTacToePlayer):
                 self.q_values[next_encoded_state] = self.INITIAL_STATE_VALUE
 
 
-    def __map_player_id(self, seat):
+    def __map_player_id(self, value):
+        """
+        Maps the symbol given by the game engine to an internal notation
+
+        @param value: the value of the seat (symbol)
+        @return: the mapped value
+        """ 
         internal_player_id = None
-        if seat:
-            if seat == self.player_id:
+        if value:
+            if value == self.player_id:
                 internal_player_id = self.COM_PLAYER_ID
             else:
                 internal_player_id = self.OPPONENT_PLAYER_ID
         return internal_player_id
 
 
-    def __reset_state(self):
+    def __reset(self):
+        """
+        Reset state of the object
+        """
         self.prev_game_state = None
 
 
     def __update_epsilon(self):
+        """
+        Update epsilon parameter
+        """
         self.epsilon *= (1 - self.epsilon_decay_step)
 
 
     def __update_q_values(self, reward):
+        """
+        Update Q-Values
+
+        @param reward: the reward for the last selected move
+        """
         if self.prev_game_state:
             learned_value = self.alpha * (reward - self.q_values[self.prev_game_state])
             self.q_values[self.prev_game_state] += learned_value
 
 
     def end_of_game(self, winning_player_id):
+        """
+        End of game. Update Q-Values and reset the game state
+
+        @param winning_player_id: the winning player ID (symbol), given by the game engine
+        """
         reward = self.DRAW_REWARD
         if winning_player_id == self.player_id:
             reward = self.COM_WIN_REWARD
         elif winning_player_id:
             reward = self.COM_LOSS_PENALTY
         self.__update_q_values(reward)
-        self.__reset_state()
+        self.__reset()
 
 
     def get_next_move(self, game_state):
+        """
+        Get the next move
+
+        @param game_state: the current game state given by the game engine
+        @return: a list with the x and y values of the selected seat
+        """
         next_move = None
         self.__init_q_values(game_state)
 
@@ -136,10 +219,20 @@ class TicTacToeCComputerPlayer(TicTacToePlayer):
 
 
     def get_q_values_from_other_com(self, com_player):
+        """
+        Merges the learned Q-Values taken by an other instance of the player
+
+        @param com_player: instance of the other player
+        """
         for game_state in com_player.q_values:
             if game_state not in self.q_values:
                 self.q_values[game_state] = com_player.q_values[game_state]
 
 
     def set_player_id(self, player_id):
+        """
+        Set the player's symbol in game
+
+        @param player_id: the symbol used by the player
+        """
         self.player_id = player_id
--- tictactoe_computer_tensorflow_player.py
+++ tictactoe_computer_tensorflow_player.py
@@ -1,13 +1,15 @@
+"""
+The neural network implementation of the Tic-Tac-Toe player component
+using Q-Learning and Tensorflow
+"""
 from __future__ import print_function
 
-import copy
-import random
 import numpy as np
 import tensorflow as tf
-
-from tictactoe_player import TicTacToePlayer
 from tensorflow.contrib import layers
 from tensorflow.python.ops import nn
+
+from tictactoe_player import TicTacToePlayer
 
 
 class TicTacToeComputerPlayer(TicTacToePlayer):
@@ -17,11 +19,22 @@ class TicTacToeComputerPlayer(TicTacToePlayer):
     DRAW_REWARD = 0.5
 
     EMPTY_SEAT = 0
+    SEAT_OCCUPIED = 1
+
     COM_PLAYER_ID = 1
     OPPONENT_PLAYER_ID = 2
 
 
     def __init__(self, epsilon=1.0, epsilon_decay_step=10e-5, board_size=3, hidden_layer_size=50):
+        """
+        Constructor
+
+        @param epsilon: the epsilon paramater (randomness of the next move)
+                        of Q-Learning algorithm [0, 1]
+        @param epsilon_decay_step: the decay factor for updating the epsilon parameter [0, 1]
+        @param board_size: a list with the number of rows and columns of the game board
+        @param hidden_layer_size: the number of nodes in the hidden layer
+        """
         self.epsilon = epsilon
         self.epsilon_decay_step = epsilon_decay_step
 
@@ -32,24 +45,22 @@ class TicTacToeComputerPlayer(TicTacToePlayer):
         self.hidden_layer_size = hidden_layer_size
 
         self.session = tf.Session()
-        self.__build_graph()
+        self.__init_graph()
 
 
-    def __get_q_values(self, splitted_states):
-        res = self.q_values_nn.eval(session=self.session, feed_dict={self.s: [splitted_states]})[0]
-        return res
+    def __init_graph(self):
+        """
+        Initialize graph
+        """
+        self.splitted_states = tf.placeholder(tf.float32, 
+                                              [None, 2, self.board_size, self.board_size], 
+                                              name="splitted_states")
 
-
-    def __update_q_values(self, s_t, a_t, y_t):
-        self.session.run(self.q_updater, feed_dict={self.s: s_t, self.a: a_t, self.y: y_t})
-
-
-    def __build_graph(self):
-        self.s = tf.placeholder(tf.float32, [None, 2, self.board_size, self.board_size], name="s")
-        net = tf.transpose(self.s, [0, 2, 3, 1])
+        net = tf.transpose(self.splitted_states, [0, 2, 3, 1])
         net = tf.reshape(net, [-1, int(np.prod(net.get_shape().as_list()[1:]))])
         net = layers.fully_connected(net, self.hidden_layer_size, activation_fn=nn.relu)
         net = layers.fully_connected(net, self.board_size*self.board_size, activation_fn=None)
+
         self.q_values_nn = tf.reshape(net, [-1, self.board_size, self.board_size])
         self.a = tf.placeholder(tf.float32, [None, self.board_size, self.board_size], name="a")
         self.y = tf.placeholder(tf.float32, [None], name="y")
@@ -61,19 +72,39 @@ class TicTacToeComputerPlayer(TicTacToePlayer):
         self.session.run(tf.initialize_all_variables())
 
 
-    def __update_epsilon(self):
-        self.epsilon *= (1 - self.epsilon_decay_step)
+    def __encode_state(self, game_state):
+        """
+        Transforms the 2D list game state into an internal representation using numpy array
+
+        @param game_state: a 2D list with the game state given by the game engine
+        @return: a 2D numpy array with the internal representation of the game state
+        """
+        encoded_state = map(self.__map_player_id, np.array(game_state).flatten())
+        encoded_state = np.array(encoded_state, float).reshape(self.board_size, self.board_size)
+        return encoded_state
 
 
-    def __reset_state(self):
-        self.player_id = None
-        self.prev_game_state = None
+    def __get_q_values(self, splitted_states):
+        """
+        Get current Q values for specific state
+
+        @param splitted_states: a numpy array with the splitted game states
+        @return: a 2D numpy array with the Q values
+        """
+        return self.q_values_nn.eval(session=self.session, 
+                                     feed_dict={self.splitted_states: [splitted_states]})[0]
 
 
-    def __map_player_id(self, seat):
+    def __map_player_id(self, value):
+        """
+        Maps the symbol given by the game engine to an internal notation
+
+        @param value: the value of the seat (symbol)
+        @return: the mapped value
+        """ 
         internal_player_id = None
-        if (seat):
-            if (seat == self.player_id):
+        if value:
+            if value == self.player_id:
                 internal_player_id = self.COM_PLAYER_ID
             else:
                 internal_player_id = self.OPPONENT_PLAYER_ID
@@ -82,7 +113,60 @@ class TicTacToeComputerPlayer(TicTacToePlayer):
         return internal_player_id
 
 
+    def __reset(self):
+        """
+        Reset state of object
+        """
+        self.player_id = None
+        self.prev_game_state = None
+
+
+    def __split_states(self, encoded_state):
+        """
+        Splitting the game state into two states, one for each player.
+        The first state refers to seats occuppied by the player itself, 
+        while the second represents the opponent's state.
+
+        @param encoded_state: the current game state in a 2D numpy array 
+        @return: a list that contains two 2D numpy arrays
+        """
+        com_state = np.copy(encoded_state)
+        com_state[com_state != self.COM_PLAYER_ID] = self.EMPTY_SEAT
+
+        opponent_state = np.copy(encoded_state)
+        opponent_state[opponent_state != self.OPPONENT_PLAYER_ID] = self.EMPTY_SEAT
+        opponent_state[opponent_state == self.OPPONENT_PLAYER_ID] = self.SEAT_OCCUPIED
+
+        return [com_state, opponent_state]
+
+
+    def __update_epsilon(self):
+        """
+        Update epsilon parameter
+        """
+        self.epsilon *= (1. - self.epsilon_decay_step)
+
+
+    def __update_q_values(self, s_t, a_t, y_t):
+        """
+        Update Q-Values
+
+        @param s_t: splitted game state
+        @param a_t: 
+        @param y_t:
+        """
+        self.session.run(self.q_updater, 
+                         feed_dict={self.splitted_states: s_t, 
+                                    self.a: a_t, 
+                                    self.y: y_t})
+
+
     def end_of_game(self, winning_player_id):
+        """
+        End of game. Update Q-Values and reset the game state
+
+        @param winning_player_id: the winning player ID (symbol), given by the game engine
+        """
         reward = self.DRAW_REWARD
         if winning_player_id == self.player_id:
             reward = self.COM_WIN_REWARD
@@ -92,29 +176,19 @@ class TicTacToeComputerPlayer(TicTacToePlayer):
         prev_splitted_states, prev_move_t = self.prev_game_state
         prev_splitted_states = [prev_splitted_states]
         prev_move_t = [prev_move_t]
-        self.__update_q_values(prev_splitted_states, prev_move_t, [reward] * len(prev_splitted_states))
+        reward = [reward] * len(prev_splitted_states)
+        self.__update_q_values(prev_splitted_states, prev_move_t, reward)
         
-        self.__reset_state()
-
-
-    def __encode_state(self, game_state):
-        encoded_state = map(self.__map_player_id, np.array(game_state).flatten())
-        encoded_state = np.array(encoded_state, float).reshape(3, 3)
-        return encoded_state
-
-
-    def __split_states(self, encoded_state):
-        com_state = np.copy(encoded_state)
-        com_state[com_state != self.COM_PLAYER_ID] = 0
-
-        opponent_state = np.copy(encoded_state)
-        opponent_state[opponent_state != self.OPPONENT_PLAYER_ID] = 0
-        opponent_state[opponent_state == self.OPPONENT_PLAYER_ID] = 1
-
-        return [com_state, opponent_state]
+        self.__reset()
 
 
     def get_next_move(self, game_state):
+        """
+        Get the next move
+
+        @param game_state: the current game state given by the game engine
+        @return: a list with the x and y values of the selected seat
+        """
         encoded_state = self.__encode_state(game_state)
 
         free_seats = np.where(encoded_state == self.EMPTY_SEAT)
@@ -136,8 +210,8 @@ class TicTacToeComputerPlayer(TicTacToePlayer):
             y_t_prev = 0. + 0.8 * q_values[best_next_move]
             prev_splitted_states = [prev_splitted_states]
             prev_move_t = [prev_move_t]
-            self.__update_q_values(prev_splitted_states, prev_move_t, [y_t_prev] * len(prev_splitted_states))
-
+            y_t_prev = [y_t_prev] * len(prev_splitted_states)
+            self.__update_q_values(prev_splitted_states, prev_move_t, y_t_prev)
 
         next_move_t = np.zeros_like(encoded_state, dtype=np.float32)
         next_move_t[next_move] = 1.
@@ -147,4 +221,9 @@ class TicTacToeComputerPlayer(TicTacToePlayer):
 
 
     def set_player_id(self, player_id):
+        """
+        Set the player's symbol in game
+
+        @param player_id: the symbol used by the player
+        """
         self.player_id = player_id
