0b5f61decd38bf3be8dd1e4883c49a6aef3a0738
Demetrios Vassiliades <dimivas@ieee.org>
2017-11-08 21:34:42 +0000

    Adding the ability to play after training

--- run.py
+++ run.py
@@ -15,11 +15,11 @@ all_players['Q-Learning'] = {'class': TicTacToeComputerQLearning, 'instance': No
 all_players['Tensorflow'] = {'class': TicTacToeComputerTensorflow, 'instance': None, 'win': 0 }
 
    
-def main(p1_class, p2_class, num_of_training_games, num_of_test_games, play_after_test=False):
+def main(p1_class, p2_class, num_of_training_games, num_of_test_games, play_after_train=False):
     start_time = datetime.now()
     all_players[p1_class]['instance'] = all_players[p1_class]['class']()
     all_players[p2_class]['instance'] = all_players[p2_class]['class']()
-    plist=[all_players[p1_class], all_players[p2_class]]
+    plist = [all_players[p1_class], all_players[p2_class]]
 
     # TRAINING GAMES
     for i in range(num_of_training_games):
@@ -40,9 +40,14 @@ def main(p1_class, p2_class, num_of_training_games, num_of_test_games, play_afte
     wins = [ x['win'] for x in plist ]
     draws = num_of_test_games - sum(wins)
     print("{}: {} - {}: {} - Draw: {}".format(p1_class, wins[0], p2_class, wins[1], draws))
-   
-    if (play_after_test): 
-        human = TicTacToeHuman()
+
+    if play_after_train:
+        time.sleep(5)
+        hlist = [TicTacToeHuman(), plist[0]['instance']]
+        while (True):
+            TicTacToeGame(*hlist, be_verbose=True).play()
+            time.sleep(5)
+            hlist.reverse()
 
 parser = argparse.ArgumentParser(description='Implementation of Tic-Tac-Toe Agents using Reinforcement Learning techniques')
 parser.add_argument('-1', '--p1',
@@ -69,8 +74,12 @@ parser.add_argument('-t', '--number-of-test-games',
                       dest='number_of_test_games',
                       default=1000,
                       help='Number of test games',)
-
+parser.add_argument('-p', '--play-after-train',
+                      action='store_true',
+                      dest='play_after_train',
+                      help='Play after training with com player 1')
 args = parser.parse_args()
 main(args.p1, args.p2, 
      args.number_of_training_games,
-     args.number_of_test_games)
+     args.number_of_test_games,
+     play_after_train=args.play_after_train)
